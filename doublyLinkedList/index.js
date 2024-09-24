50d7393ce3300d6be593379221b56ce1498e42b0
Rahul Durge <rahul222durge@gmail.com>
2024-09-24 15:32:40 +0530

    add unshift method

--- doublyLinkedList/index.js
+++ doublyLinkedList/index.js
@@ -1,7 +1,73 @@
-class Node{
-    constructor(value){
-        this.value = value ;
-        this.next = null ;
-        this.
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
     }
 }
+
+class doublyLinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        this.tail.next = newNode;
+        newNode.prev = this.tail
+        this.tail = newNode;
+        this.length++
+        return this;
+    }
+
+    pop(){
+        let temp = this.tail ;
+        if (!this.head) {
+            return console.log("No head is present to pop()");
+            
+        }
+
+        if (this.length === 1) {
+            this.head  = null ;
+            this.tail = null ;
+        }
+        this.tail = this.tail.prev ;
+        this.tail.next = null ;
+        temp.prev = null ;
+        this.length-- ;
+        return temp ;
+    }
+
+    unshift(value){
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        let temp = this.head;
+        temp.prev = newNode ;
+        newNode.prev = null ;
+        newNode.next =  temp; 
+        this.head = newNode;
+        
+        this.length++ ;
+        return temp;
+
+    }
+
+}
+
+const myDoublyLinkedList = new doublyLinkedList(1)
+myDoublyLinkedList.push(2)
+myDoublyLinkedList.push(3)
+myDoublyLinkedList.push(4)
+myDoublyLinkedList.pop()
+myDoublyLinkedList.unshift(0)
+console.log(myDoublyLinkedList);
